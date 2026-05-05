12199ed0fb56b186edace01fb8a6e349fa6a2350
Martin <martin.sueldo@alumnos.frgp.utn.edu.ar>
2026-05-05 15:59:09 -0300

    subo ListarSeguroServlet terminado con metodos doGet y doPost

--- src/main/java/servlets/ListarSeguroServlet.java
+++ src/main/java/servlets/ListarSeguroServlet.java
@@ -1,7 +1,9 @@
 package servlets;
 
 import dao.SeguroDAO;
+import dao.TipoSeguroDAO;
 import dominio.Seguro;
+import dominio.TipoSeguro;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,21 +15,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/ListarSeguroServlet")
 public class ListarSeguroServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     
     public ListarSeguroServlet() {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		SeguroDAO seguroDAO = new SeguroDAO();
-		ArrayList<Seguro> lista = seguroDAO.listarSeguros(0);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        
+        TipoSeguroDAO tipoDAO = new TipoSeguroDAO();
+        ArrayList<TipoSeguro> tipos = tipoDAO.obtenerTodos();
+        request.setAttribute("tipos", tipos);
+        
+        int idFiltro = 0;       
+        
+        String paramFiltro = request.getParameter("ddlFiltroTipo"); 
+        
+       
+        if (paramFiltro != null && !paramFiltro.isEmpty()) {
+            idFiltro = Integer.parseInt(paramFiltro);
+        }
+        
+        SeguroDAO seguroDAO = new SeguroDAO();
+        ArrayList<Seguro> lista = seguroDAO.listarSeguros(idFiltro);
+        request.setAttribute("lista", lista);
 
-		request.setAttribute("lista", lista);
-		RequestDispatcher rd = request.getRequestDispatcher("ListarSeguro.jsp");
-		rd.forward(request, response);
-	}
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/ListarSeguro.jsp"); 
+        rd.forward(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
